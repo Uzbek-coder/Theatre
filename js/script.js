8558866d2abb6fe5530cd0ff52e14e7e0c53e629
Uzbek-coder <shamsidinovm7@gmail.com>
2021-05-09 17:05:52 +0500

    Theatre

--- js/script.js
+++ js/script.js
@@ -4,6 +4,30 @@ new Swiper('.image-slider', {
 		nextEl: '.btn-right',
 		prevEl: '.btn-left'
 	},
+
+	loop: true,
+});
+
+new Swiper('.slide__show-slider', {
+	
+	slidesPerView: 4,
+
+	breakpoints: {
+		1300: {
+			slidesPerView: 4,
+		},
+		1220: {
+			slidesPerView: 3,
+		},
+		800: {
+			slidesPerView: 2,
+		},
+		320: {
+			slidesPerView: 1,
+		}
+	},
+
+	loop: true,
 });
 
 const burger = document.querySelector('.menu__icon'),
@@ -11,6 +35,7 @@ const burger = document.querySelector('.menu__icon'),
 	links = document.querySelectorAll('.menu__list li');
 function menu () {
 	burger.addEventListener('click', () => {
+		document.body.classList.toggle('_lock');
 		nav.classList.toggle('nav-active');
 		burger.classList.toggle('toggle');
 		links.forEach((link, index) => {
